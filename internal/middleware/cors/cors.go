6fd2ae8dd5a810b504819eaf558468ccbc025207
Arif14377 <arifrahman14377@gmail.com>
2026-04-13 21:11:10 +0700

    fix: change the CORS configuration from hardcoded to via env variable calls

--- internal/middleware/cors/cors.go
+++ internal/middleware/cors/cors.go
@@ -2,14 +2,16 @@ package cors
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Middleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// c.Writer.Header().Set("Access-Control-Allow-Origin", "http://68.183.226.223:20101") // production
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173") // development
+		FRONTEND_URL := os.Getenv("FRONTEND_URL")
+
+		c.Writer.Header().Set("Access-Control-Allow-Origin", FRONTEND_URL) // development
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PATCH")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
